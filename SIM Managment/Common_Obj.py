28063870c0094cf816834fc4619d44272f601db1
RAKwireless-HK <kevin.cheng@rakwireless.com>
2024-08-06 09:37:58 +0800

    Update Common_Obj.py

--- SIM Managment/Common_Obj.py
+++ SIM Managment/Common_Obj.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 class SIM_Info:
     def __init__(self, Project, SIM_ICCID, SIM_MRT, SIM_CARRIER, SIM_APN, SIM_IP, ACTIVE_Date):
-        self.ORGANIZATION = "EMSD"
+        self.ORGANIZATION = [PROJECT_ORGANIZATION]
         self.PROJECT = Project
         self.SIM_ICCID = SIM_ICCID
         self.SIM_MRT = SIM_MRT # Mobile Phone number
@@ -12,7 +12,7 @@ class SIM_Info:
         self.ACTIVE_Date = ACTIVE_Date
         # self.END_Date = ACTIVE_Date + 365 days
         self.END_Date = ACTIVE_Date + timedelta(days=365)
-        self.SIM_OWNER = "Keith Mak"
+        self.SIM_OWNER = [SIM_OWNER_NAME]
 
 class GStack_Webpage:
     def __init__(self, _gstack):
